b700ceedf894fc8a0520b769cfcc3329e8af29cd
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-09-18 12:29:01 +0200

    add currency converter js, index and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "./Container";
+import Title from "./Title";
+import RequiredText from "./RequiredText";
+import Form from "./Form";
+import Input from "./Input";
+import Button from "./Button";
+import Result from "./Result";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <form>
+        <fieldset className="form__fieldset">
+          <Title />
+          <RequiredText />
+          <Form />
+          <Input />
+          <Button />
+          <Result />
+        </fieldset>
+      </form>
+    </Container>
   );
 }
 
