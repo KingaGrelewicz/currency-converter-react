d2e209498b243d47eab0cdbed87e6f0494905f88
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-09-29 16:42:34 +0200

    Input moved to Form, and Button to Result. currency table added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import { useState } from 'react';
 import Container from "./Container";
 import Title from "./Title";
 import RequiredText from "./RequiredText";
 import Form from "./Form";
-import Input from "./Input";
-import Button from "./Button";
 import Result from "./Result";
 
-
 function App() {
+  const [currencies, setCurrency] = useState([
+    { id: 1, name: "Euro", value: 4.6 },
+    { id: 2, name: "Dolar amerykański", value: 4.32 },
+    { id: 3, name: "Funt brytyjski", value: 5.30 },
+  ]);
+
+
   return (
     <Container>
-      <form>
-        <fieldset className="form__fieldset">
-          <Title />
-          <RequiredText />
-          <Form />
-          <Input />
-          <Button />
-          <Result />
-        </fieldset>
-      </form>
+      <fieldset className="form__fieldset">
+        <Title />
+        <RequiredText />
+        <Form currencies={currencies} />
+        <Result 
+          currencies={currencies}
+        />
+      </fieldset>
     </Container>
   );
 }
