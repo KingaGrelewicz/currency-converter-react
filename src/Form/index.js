3f2859adfb5aabd169c305a1487c848c2079e016
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-09-29 16:43:22 +0200

    functions added to Form and Result

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,51 @@
 import "./style.css";
+import { useState } from 'react';
 
-const Form = () => (
-  <section>
-    <label className="form__labelName"> Wybierz walutę:
-      <select className="form__element" name="selectElement" id="currencyElement">
-        <option value="EUR" selected>Euro</option>
-        <option value="USD">Dolar amerykański</option>
-        <option value="CHF">Frank szwajcarski</option>
-        <option value="JPY">Jen</option>
-        <option value="CZK">Korona czeska</option>
-        <option value="NOK">Korona norweska</option>
-      </select>
-    </label>
-  </section>
-)
+const Form = ({ currencies }) => {
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const onSelectChange = ({ target }) => setSelectedCurrency(target.value);
+  const onAmountChange = ({ target }) => setAmount(target.value);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <label className="form__labelName"> Wybierz walutę:
+        <select
+          value={selectedCurrency}
+          onChange={onSelectChange}
+          className="form__element"
+          name="selectElement"
+        >
+          <option value="">Wybierz walutę</option>
+          {currencies?.map(currency => (
+            <option key={currency.id} value={currency.value}>
+              {currency.name}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="form__labelName">
+        Kwota w złotówkach*:
+        <input
+          value={amount}
+          onChange={onAmountChange}
+          className="form__element"
+          type="number"
+          name="amountElement"
+          id="amountElement"
+          min="10"
+          placeholder="100"
+          step="any"
+          required
+          autoFocus />
+      </label>
+    </form>
+  )
+}
 
 export default Form;
--- src/Result/index.js
+++ src/Result/index.js
@@ -1,9 +1,28 @@
 import "./style.css";
+import { useState } from 'react';
 
-const Result = () => (
-    <label
-        className="result">Twoja kwota: <strong>__</strong>
-    </label>
-)
+const Result = ({ currencies, amount, selectedCurrency }) => {
+   const [result, setResult] = useState("");
+
+   const calculateResult = () => {
+      const selectedCurrency = currencies.find((currency) => currency.id === selectedCurrency);
+
+      if (selectedCurrency) {
+         const calculatedResult = amount.value / selectedCurrency.value;
+         setResult(calculatedResult);
+      }
+   };
+
+   return (
+      <>
+         <button onClick={calculateResult} className="form__button">Przelicz</button>
+         <span
+            className="result"
+         >
+            {result}
+         </span>
+      </>
+   )
+}
 
 export default Result;
