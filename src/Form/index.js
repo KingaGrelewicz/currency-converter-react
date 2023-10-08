ef01e5c470a5288d2488316914c4ce971032d1c2
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-10-08 23:06:07 +0200

    corrected Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,49 +1,67 @@
 import "./style.css";
+import { useState } from 'react';
+import { currencies } from "./currencies";
+import { Result } from "./Result";
 
-const Form = ({ currencies, selectedCurrency, amount, setSelectedCurrency, setAmount, calculateResult }) => {
+export const Form = ({ result, calculateResult }) => {
+  const [currency, setCurrency] = useState("");
+  const [amount, setAmount] = useState("");
 
-  const onSelectChange = ({ target }) => setSelectedCurrency(target.value);
+  const onSelectChange = ({ target }) => setCurrency(target.value);
   const onAmountChange = ({ target }) => setAmount(target.value);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+    calculateResult(currency, amount);
   };
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <label className="form__labelName"> Wybierz walutę:
-        <select
-          value={selectedCurrency}
-          onChange={onSelectChange}
-          className="form__element"
-          name="selectElement"
-        >
-          <option value="">Wybierz walutę</option>
-          {currencies?.map(currency => (
-            <option key={currency.id} value={currency.id}>
-              {currency.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label className="form__labelName">
-        Kwota w złotówkach*:
-        <input
-          value={amount}
-          onChange={onAmountChange}
-          className="form__element"
-          type="number"
-          name="amountElement"
-          id="amountElement"
-          min="10"
-          placeholder="100"
-          step="any"
-          required
-          autoFocus />
-      </label>
-      <button onClick={calculateResult} className="form__button">Przelicz</button>
+    <form className="form" onSubmit={onFormSubmit}>
+      <h1 className="form__name">Kalkulator walut</h1>
+      <p className="form__requiredText">
+        Pola wymagane oznaczone są *
+      </p>
+      <p>
+        <label className="form__labelName"> Wybierz walutę:
+          <select
+            value={currency}
+            onChange={onSelectChange}
+            className="form__element"
+            name="selectElement"
+          >
+            <option value="">Wybierz walutę</option>
+            {currencies.map((currency => (
+              <option
+                key={currency.short}
+                value={currency.short}
+              >
+                {currency.name}
+              </option>
+            )))}
+          </select>
+        </label>
+      </p>
+      <p>
+        <label className="form__labelName">
+          Kwota w złotówkach*:
+          <input
+            value={amount}
+            onChange={onAmountChange}
+            className="form__element"
+            type="number"
+            name="amountElement"
+            id="amountElement"
+            min="10"
+            placeholder="100"
+            step="any"
+            required
+            autoFocus />
+        </label>
+      </p>
+      <p>
+        <button className="form__button">Przelicz</button>
+      </p>
+      <Result result={result} />
     </form>
   )
 }
-
-export default Form;
