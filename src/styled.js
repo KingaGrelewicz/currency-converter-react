972ab739b7ace7215db3041b420367a046516182
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-10-29 16:14:47 +0100

    globla styled added

---
+++ src/styled.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background01.jpg";
+
+export const GlobalStyled = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        background-image: url("${background}");
+        background-size: cover;
+    }
+`;
