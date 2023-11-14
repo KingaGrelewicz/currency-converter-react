eeb90330964b9f14cfbb4b8d6e25f3d88d93c697
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-11-14 11:38:56 +0100

    custom hook - useRatesData created

---
+++ src/Form/useRatesData.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({
+        rates: null,
+        loading: false,
+        error: "Ups coś poszło nie tak, odśwież stronę",
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setRatesData((prevData) => ({
+                    ...prevData,
+                    loading: true,
+                  }));
+
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+
+                const response = await axios.get("rates.json");
+                  
+                setRatesData({
+                    rates: response.data,
+                    loading: false,
+                    error: null,
+                  });
+
+            } catch (error) {
+                console.error(error);
+                setRatesData({
+                    rates: null,
+                    loading: false,
+                    error: error.message,
+                });
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return ratesData;
+}
