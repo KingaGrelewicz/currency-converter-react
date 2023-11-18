ec98fc05ae54d1f4287e47e0cd344d59c8c31135
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-11-18 21:24:28 +0100

    ratesData state corrected

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ import { GlobalStyled } from "./styled";
 import { useRatesData } from './Form/useRatesData';
 
 function App() {
-  const [result, setResult] = useState();
-  const { rates, loading, error } = useRatesData();
- 
+  const [result, setResult] = useState("");
+  const { ratesData, status  } = useRatesData("");
+   
   const calculateResult = (currency, amount) => {
-       const rate = rates.data[currency];
-    
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
+    if (status === "loading") {
+      console.log("Dane o kursach są jeszcze ładowane. Poczekaj chwilę.");
+      return;
+    }
+    if (ratesData && ratesData.status === "success" && ratesData.data) {
+      const currencyData = ratesData.data.find(rate => rate.code === currency);
+  
+      if (currencyData) {
+        setResult({
+          sourceAmount: +amount,
+          targetAmount: amount / currencyData.value,
+          currency,
+        });
+      };
+    } else {
+      console.error(`Nie można znaleźć kursu dla waluty ${currency}`);
+    }
   }
 
   return (
-      <StyledContainer>
-        <GlobalStyled />
-        <Form
-          result={result}
-          calculateResult={calculateResult}
-        />
-      </StyledContainer>
+    <StyledContainer>
+      <GlobalStyled />
+      <Form
+        result={result}
+        calculateResult={calculateResult}
+        ratesData={ratesData}
+      />
+    </StyledContainer>
   );
 }
 
--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -3,35 +3,25 @@ import axios from 'axios';
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState({
-        rates: null,
-        loading: false,
-        error: "Ups coś poszło nie tak, odśwież stronę",
+        status: "loading",
+        data: null
     });
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                setRatesData((prevData) => ({
-                    ...prevData,
-                    loading: true,
-                  }));
-
                 await new Promise((resolve) => setTimeout(resolve, 1000));
 
                 const response = await axios.get("../rates.json");
-                  
-                setRatesData({
-                    rates: response.data,
-                    loading: false,
-                    error: null,
-                  });
 
-            } catch (error) {
-                console.error(error);
                 setRatesData({
-                    rates: null,
-                    loading: false,
-                    error: error.message,
+                    status: "success",
+                    data: Object.values(response.data.data),
+                });
+            } catch {
+                setRatesData({
+                    status: "error",
+                    data: null,
                 });
             }
         };
