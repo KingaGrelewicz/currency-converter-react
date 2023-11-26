cd3471046f4141673838b23b4986358472a3b364
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-11-26 13:34:49 +0100

    footer props corrected

--- src/Form/Footer/index.js
+++ src/Form/Footer/index.js
@@ -2,11 +2,11 @@ import { FooterContainer } from "./styled";
 import { useRatesData } from '../useRatesData';
 
 export const Footer = () => {
-    const { formattedDate } = useRatesData();
+    const { date } = useRatesData();
 
     return (
         <FooterContainer>
-            Kursy walut zostały pobrane z app.currencyapi.com w dniu {formattedDate}
+            Kursy walut zostały pobrane z app.currencyapi.com w dniu {date}
         </FooterContainer>
     )
 };
