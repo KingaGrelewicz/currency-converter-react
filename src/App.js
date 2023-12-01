e12598000086519239284a94d8e105a294e962f1
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-01 17:42:33 +0100

    deleted useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import StyledContainer from "./Container";
 import { Form } from "./Form";
 import { GlobalStyled } from "./styled";
@@ -7,35 +7,28 @@ import { useRatesData } from "./Form/useRatesData";
 function App() {
   const [result, setResult] = useState("");
   const { ratesData } = useRatesData("");
-  
-  const calculateResult = (currency, amount) => {
+
+  const calculateResult = (amount, currency) => {
     console.log("ratesData:", ratesData);
 
-    if (ratesData && ratesData.data) {
-      const currencyData = ratesData.data.find(rate => rate.code === currency);
+    if (ratesData && ratesData.currencies && ratesData.rate) {
+      const currencyIndex = ratesData.currencies.findIndex(code => code === currency);
 
-      try {
-        if (currencyData !== undefined) {
-          const targetAmount = amount / currencyData.value;
-          console.log("targetAmount:", targetAmount);
+      if (currencyIndex !== -1) {
+        const currencyValue = ratesData.rate[currencyIndex];
 
+        try {
           setResult({
             sourceAmount: +amount,
-            targetAmount: targetAmount,
+            targetAmount: amount / currencyValue,
             currency,
           });
-        } else {
-          console.error(`Nie można znaleźć kursu dla waluty ${currency}`);
+        } catch (error) {
+          console.error(error);
         }
-      } catch (error) {
-        console.error(error);
       }
     }
   }
-
-  useEffect(() => {
-    console.log("Przed calculateResult - ratesData:", ratesData);
-  }, [ratesData]);
 
   return (
     <StyledContainer>
