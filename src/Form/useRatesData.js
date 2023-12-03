f1ddb3c9ab1948e9a777101b091c925ab4bab46e
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-03 15:09:19 +0100

    date moved to setRatesData

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -13,17 +13,14 @@ export const useRatesData = () => {
         const fetchData = async () => {
             try {
                 await new Promise((resolve) => setTimeout(resolve, 1000));
-
-                const response = await axios.get("/rates.json");
-                const metaDate = response.data.meta.last_updated_at;
-                const formattedDate = new Date(metaDate).toLocaleString();
+                const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_etULAae4hL6redqwftsRZRwwZliL1twfy51hbI5m&currencies=EUR%2CUSD%2CCAD");
 
                 setRatesData({
                     status: "success",
                     currencies: Object.keys(response.data.data)
                         .map(key => response.data.data[key].code),
                     rate: Object.values(response.data.data).map(item => item.value),
-                    date: formattedDate,
+                    date: new Date(response.data.meta.last_updated_at).toLocaleString(),
                 });
 
             } catch (error) {
