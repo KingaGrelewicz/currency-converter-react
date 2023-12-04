8e9207bc7b4ffe913cc15674c436c517833c3c65
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-04 13:57:23 +0100

    ratesData object simplified

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,43 +1,37 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+    import { useEffect, useState } from 'react';
+    import axios from 'axios';
 
-export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({
-        status: "loading",
-        currencies: null,
-        rate: null,
-        date: null,
-    });
+    const apiAddress = ("/rates.json");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-                const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_etULAae4hL6redqwftsRZRwwZliL1twfy51hbI5m&currencies=EUR%2CUSD%2CCAD&base_currency=PLN");
+    export const useRatesData = () => {
+        const [ratesData, setRatesData] = useState({
+            status: "loading",
+        });
 
-                setRatesData({
-                    status: "success",
-                    currencies: Object.keys(response.data.data)
-                        .map(key => response.data.data[key].code),
-                    rate: Object.values(response.data.data).map(item => item.value),
-                    date: new Date(response.data.meta.last_updated_at).toLocaleString(),
-                });
+        useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
+                    const response = await axios.get(apiAddress);
 
-            } catch (error) {
-                console.error(error);
-                setRatesData({
-                    status: "error",
-                    currencies: null,
-                    rate: null,
-                    date: null,
-                });
-            }
+                    setRatesData({
+                        status: "success",
+                        currencies: response.data.data,
+                        date: new Date(response.data.meta.last_updated_at).toLocaleString(),
+                    });
+
+                } catch (error) {
+                    console.error(error);
+                    setRatesData({
+                        status: "error",
+                    });
+                }
+            };
+
+            fetchData();
+        }, []);
+
+        return {
+            ...ratesData,
         };
-
-        fetchData();
-    }, []);
-
-    return {
-        ...ratesData,
-    };
-}
+    }
