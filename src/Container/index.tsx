88edadc77213e3354678d484b75cea0caae30784
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-09-27 10:29:50 +0200

    Container component converted into Type Script

---
+++ src/Container/index.tsx
@@ -0,0 +1,11 @@
+import { StyledContainer } from "./styled";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => (
+  <StyledContainer>{children}</StyledContainer>
+);
+
+export default Container;
