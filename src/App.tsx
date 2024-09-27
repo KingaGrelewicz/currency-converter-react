a04e89ad26e24c7db32a59c815a233023b1083ae
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-09-27 10:28:17 +0200

    App component converted into Type Script

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,29 @@ import StyledContainer from "./Container";
 import { Form } from "./Form";
 import { GlobalStyled } from "./styled";
 import { useRatesData } from "./Form/useRatesData";
+import { useCurrentDate } from './Form/Clock/useCurrentDate';
+
+type CurrencyResult = {
+  sourceAmount: number;
+  targetAmount: number;
+  currency: string;
+} | null;
 
 function App() {
-  const [result, setResult] = useState();
-  const [isResultVisible, setIsResultVisible] = useState(false);
+  const [result, setResult] = useState<CurrencyResult>(null);
+  const [isResultVisible, setIsResultVisible] = useState<boolean>(false);
   const ratesData = useRatesData();
+  const currentDate = useCurrentDate();
 
-  const calculateResult = (currency, amount) => {
-    const currencies = ratesData.currencies[currency];
+  const calculateResult = (currency: string, amount: number) => {
+    const currencies = ratesData.currencies?.[currency];
 
-    try {
-      const currencyData = currencies && currencies.value;
+    if (!currencies) {
+      console.error("Currency data not available.");
+      return;
+    }
+
+      const currencyData = currencies;
 
       setResult({
         sourceAmount: +amount,
@@ -22,10 +34,8 @@ function App() {
       });
 
       setIsResultVisible(true);
-    } catch (error) {
-      console.error(error);
     }
-  }
+
 
   return (
     <StyledContainer>
@@ -35,6 +45,7 @@ function App() {
         calculateResult={calculateResult}
         ratesData={ratesData}
         isResultVisible={isResultVisible}
+        currentDate={currentDate}
       />
     </StyledContainer>
   );
